20bf71e832274cde0b12a197d2c216ea37495435
LiamJonson <liamJonson@rambler.ru>
2019-10-25 15:52:44 +0300

    Implementation of Login Page. add to cart from product page

--- pages/locators.py
+++ pages/locators.py
@@ -5,6 +5,14 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators ():
+class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     LOGIN_REG = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocators():
+    BASKET_ADD_BTN = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_BASKET = (By.CSS_SELECTOR, "div.alertinner ")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    BASKET_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
---
+++ test_product_page.py
@@ -0,0 +1,8 @@
+from pages.product_page import ProductPage
+
+
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_buy_product()
